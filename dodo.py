da08a8f5e115d8ee537cc13d59516a495c904f96
Mike Pagel <mike@mpagel.de>
2017-08-14 20:19:25 +0200

    allowed providing manual YAML to prevent usage of text recognition

--- dodo.py
+++ dodo.py
@@ -33,46 +33,52 @@ def task_extract():
         pdf_paths = glob.iglob(os.path.join(input_dir, '**', '*.pdf'), recursive=True)
 
         for pdf_path in pdf_paths:
-            rel_path = os.path.relpath(pdf_path, input_dir)
-            rel_base_path, _ = os.path.splitext(rel_path)
+            base_path, _ = os.path.splitext(pdf_path)
+            rel_base_path = os.path.relpath(base_path, input_dir)
             out_base_path = os.path.join(OUTPUT_DIR, str(index), rel_base_path)
 
-            txt_path = out_base_path + '.txt'
-            dirname = os.path.dirname(txt_path)
+            # allow usage of manually created annotation yaml:
+            manual_yml_path = base_path + '.yml'
+            if os.path.exists(manual_yml_path):
+                _logger.debug(f'Using manual annotation {manual_yml_path!r}.')
+                yml_paths.append(manual_yml_path)
+            else:
+                txt_path = out_base_path + '.txt'
+                dirname = os.path.dirname(txt_path)
 
-            if dirname not in created_dirs:
-                created_dirs.add(dirname)
+                if dirname not in created_dirs:
+                    created_dirs.add(dirname)
+
+                    yield dict(
+                        name=dirname,
+                        clean=True,
+                        targets=[dirname],
+                        actions=[(doit.tools.create_folder, (dirname,))],
+                        uptodate=[doit.tools.run_once]
+                    )
 
                 yield dict(
-                    name=dirname,
+                    name=txt_path,
+                    file_dep=[pdf_path],
+                    task_dep=['extract:' + dirname],
+                    targets=[txt_path],
                     clean=True,
-                    targets=[dirname],
-                    actions=[(doit.tools.create_folder, (dirname,))],
-                    uptodate=[doit.tools.run_once]
+                    actions=[[PDFTOTEXT, '-table', pdf_path, txt_path]],
                 )
 
-            yield dict(
-                name=txt_path,
-                file_dep=[pdf_path],
-                task_dep=['extract:' + dirname],
-                targets=[txt_path],
-                clean=True,
-                actions=[[PDFTOTEXT, '-table', pdf_path, txt_path]],
-            )
-
-            yml_path = out_base_path + '.yml'
-            yield dict(
-                name=yml_path,
-                file_dep=[txt_path],
-                targets=[yml_path],
-                clean=True,
-                actions=[(recognize_pdf_text, (), {
-                    'pdf_path': pdf_path,
-                    'txt_path': txt_path,
-                    'yml_path': yml_path,
-                })],
-            )
-            yml_paths.append(yml_path)
+                yml_path = out_base_path + '.yml'
+                yield dict(
+                    name=yml_path,
+                    file_dep=[txt_path],
+                    targets=[yml_path],
+                    clean=True,
+                    actions=[(recognize_pdf_text, (), {
+                        'pdf_path': pdf_path,
+                        'txt_path': txt_path,
+                        'yml_path': yml_path,
+                    })],
+                )
+                yml_paths.append(yml_path)
 
     xlsx_path = os.path.join(OUTPUT_DIR, 'expenses.xlsx')
     yield dict(
